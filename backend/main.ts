964cba4d22ca5d424e15df3b1ad071f4539c819d
Nandisha <61039123+AlexFreik@users.noreply.github.com>
2026-03-17 03:08:06 +0000

    switched to esm

--- backend/main.ts
+++ backend/main.ts
@@ -1,18 +1,24 @@
-import { app, BrowserWindow } from 'electron';
-import { ipcMain, dialog } from 'electron';
+import { app, BrowserWindow, ipcMain, dialog } from 'electron';
 import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const configPath = path.join(app.getPath('userData'), 'config.json');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function createWindow(): void {
+    console.log(__dirname, path.join(__dirname, 'preload.js'));
     const win = new BrowserWindow({
         width: 1200,
         height: 800,
-        icon: path.join(__dirname, '../../ui/icon.ico'),
+        icon: path.join(__dirname, '../ui/icon.ico'),
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
         },
     });
 
-    win.loadFile(path.join(__dirname, '../../ui/index.html'));
+    win.loadFile(path.join(__dirname, '../ui/index.html'));
 }
 
 app.whenReady().then(createWindow);
@@ -26,10 +32,16 @@ app.on('window-all-closed', () => {
 ipcMain.handle('select-vmix-file', async () => {
     const result = await dialog.showOpenDialog({
         properties: ['openFile'],
-        filters: [{ name: 'vMix Presets', extensions: ['vmix'] }],
+        filters: [{ name: 'vMix Preset', extensions: ['vmix'] }],
     });
 
     if (result.canceled) return null;
 
-    return result.filePaths[0];
+    const vmixPath = result.filePaths[0];
+
+    // save to config
+    fs.writeFileSync(configPath, JSON.stringify({ vmixPath }));
+    console.log(configPath);
+
+    return vmixPath;
 });
