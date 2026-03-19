67396fdc15d1547d8fc3e48e33cb93d01e3a7056
Nandisha <61039123+AlexFreik@users.noreply.github.com>
2026-03-19 22:28:57 +0000

    bug fixes

--- backend/preset-generator.ts
+++ backend/preset-generator.ts
@@ -24,9 +24,11 @@ function getFullXML(xml: string, inputs: string[]) {
 
 export function createPresetFile(folderPath: string, baseFilePath: string) {
     const baseXML = fs.readFileSync(baseFilePath, 'utf-8');
+    console.log('Read base xml file');
 
     const micId = getTitleId(baseXML, 'Mic');
     const camId = getTitleId(baseXML, 'Cam');
+    console.log('Identrified micId: ' + micId + ' and camId: ' + camId);
 
     const fileMap = getFolderFiles(folderPath);
 
@@ -39,22 +41,30 @@ export function createPresetFile(folderPath: string, baseFilePath: string) {
     fileMap.delete(-1);
 
     const config = getConfigFile(folderPath);
-    const sortedKeys = Array.from(fileMap.keys()).sort();
+    const sortedKeys = Array.from(fileMap.keys()).sort((a, b) => a - b);
+
     for (const key of sortedKeys) {
         const files = fileMap.get(key)!;
         const options = config.get(key) ?? [];
         const layers: string[] = [];
         if (micId && options.includes('mic')) layers.push(micId);
 
-        console.assert(files.length > 0);
-        if (files.length === 1) {
-            inputsXML.push(getFileXML(files[0], layers, config.get(key)));
-            continue;
-        }
         const audios = files.filter((f) => f.type === FILE_TYPES.AUDIO);
         const videos = files.filter((f) => f.type === FILE_TYPES.VIDEO);
         const images = files.filter((f) => f.type === FILE_TYPES.IMAGE);
         const slideshows = files.filter((f) => f.type === FILE_TYPES.FOLDER);
+
+        console.assert(files.length > 0);
+        if (files.length === 1) {
+            if (images.length > 0 && options.includes('cam') && camId) {
+                layers.push(images[0].id);
+                inputsXML.push(getVirtualInput({ id: camId, newId: crypto.randomUUID() }, layers));
+                helperInputsXML.push(getFileXML(images[0], [], [...options, 'collapsed']));
+            } else {
+                inputsXML.push(getFileXML(files[0], layers, config.get(key)));
+            }
+            continue;
+        }
 
         // Ignore any strange cases
         if (audios.length + videos.length > 1 || images.length + slideshows.length > 1) {
@@ -69,9 +79,6 @@ export function createPresetFile(folderPath: string, baseFilePath: string) {
                 helperInputsXML.push(getFileXML(top, [], [...options, 'collapsed']));
             }
             inputsXML.push(getFileXML(base, layers, options));
-        } else if (images.length > 0 && options.includes('cam') && camId) {
-            layers.push(images[0].id);
-            getVirtualInput({ id: camId, newId: crypto.randomUUID() }, layers);
         } else {
             files.forEach((f) => inputsXML.push(getFileXML(f, layers, options)));
         }
@@ -143,6 +150,19 @@ function getFolderFiles(folderPath: string) {
 
 // ===== XML Inputs =====
 
+function escapeXML(str: string): string {
+    return str
+        .replace(/&/g, '&amp;')
+        .replace(/"/g, '&quot;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/'/g, '&apos;');
+}
+
+function getLayersText(layers: string[]) {
+    return layers.map((layer, i) => `Overlay${i}="${layer}"`).join(' ');
+}
+
 function getAudioOptions(options: string[]) {
     const token = options.find((opt) => opt.endsWith('%')) ?? '100%';
     let volume = parseInt(token.replace('%', ''));
@@ -177,17 +197,16 @@ function getImageXML(
     layers: string[] = [],
     options: string[] = [],
 ) {
-    const layersText = layers.map((layer, i) => `Overlay${i}="${layer}"`).join('');
     const collapsed = options.includes('collapsed') ? 'True' : 'False';
 
-    return `<Input Type="1" Position="0" RangeStart="0" RangeStop="0" State="1" OriginalTitle="${path.basename(file.path)}"
+    return `<Input Type="1" Position="0" RangeStart="0" RangeStop="0" State="1" OriginalTitle=""
       ShortcutMappings="" Key="${file.id}" Loop="False" VolumeF="1" Muted="True"
       BalanceF="0" AspectRatio="100" Category="0" MouseClickAction="0" GOClickAction="20" Collapsed="${collapsed}"
       Solo="False" BusMVolumeF="1" HeadphonesVolumeF="1" BusAVolumeF="1" BusBVolumeF="1" BusCVolumeF="1"
       BusDVolumeF="1" BusEVolumeF="1" BusFVolumeF="1" BusGVolumeF="1" BusMaster="True" FrameDelay="0"
       TallyNumber="0" AutoAudioMixing="True" AutoPause="True" AutoRestart="False" AutoPlay="True" Mirror="False"
-      SelectedIndex="0" Rate="1" FlattenLayers="False" ${layersText} VideoShader_ClippingX1="0"
-      VideoShader_ClippingX2="1" VideoShader_ClippingY1="0" VideoShader_ClippingY2="1">${file.path}</Input>`;
+      SelectedIndex="0" Rate="1" FlattenLayers="False" ${getLayersText(layers)} VideoShader_ClippingX1="0"
+      VideoShader_ClippingX2="1" VideoShader_ClippingY1="0" VideoShader_ClippingY2="1">${escapeXML(file.path)}</Input>`;
 }
 
 function getVideoXML(
@@ -195,11 +214,10 @@ function getVideoXML(
     layers: string[] = [],
     options: string[] = [],
 ) {
-    const layersText = layers.map((layer, i) => `Overlay${i}="${layer}"`).join('');
     const { volume, gain } = getAudioOptions(options);
     const loop = options.includes('loop') ? 'True' : 'False';
 
-    return `<Input Type="0" Position="0" RangeStart="0" RangeStop="0" State="1" OriginalTitle="${path.basename(file.path)}"
+    return `<Input Type="0" Position="0" RangeStart="0" RangeStop="0" State="1" OriginalTitle=""
         ShortcutMappings="" Key="${file.id}" Loop="${loop}" VolumeF="${volume}" Muted="True" BalanceF="0" AspectRatio="100" Category="0"
         MouseClickAction="0" GOClickAction="20" Collapsed="False" SoloPFLMode="False" MetersPF="False" Solo="False" BusMVolumeF="1"
         HeadphonesVolumeF="1" BusAVolumeF="1" BusBVolumeF="1" BusCVolumeF="1" BusDVolumeF="1" BusEVolumeF="1" BusFVolumeF="1" BusGVolumeF="1"
@@ -208,7 +226,7 @@ function getVideoXML(
         AudioNoiseGateThreshold="0" AudioEQEnabled="False" AudioEQGainDB0="0" AudioEQGainDB1="0" AudioEQGainDB2="0" AudioEQGainDB3="0"
         AudioEQGainDB4="0" AudioEQGainDB5="0" AudioEQGainDB6="0" AudioEQGainDB7="0" AudioEQGainDB8="0" AudioEQGainDB9="0" AudioAGCEnabled="False"
         AudioRackXML="&lt;plugins /&gt;" BusMaster="True" BusA="True" FrameDelay="0" TallyCOMPort="None" TallyNumber="0" AutoAudioMixing="True"
-        AutoPause="True" AutoRestart="True" AutoPlay="True" Mirror="False" SelectedIndex="0" Rate="1" FlattenLayers="False" ${layersText} XML=""
+        AutoPause="True" AutoRestart="True" AutoPlay="True" Mirror="False" SelectedIndex="0" Rate="1" FlattenLayers="False" ${getLayersText(layers)} XML=""
         ShaderSource="00000000-0000-0000-0000-000000000000" PTZProvider="" PTZConnection="" PTZAutoConnect="False" PTZDefaultPanTiltSpeed="0.5"
         PTZDefaultZoomSpeed="0.5" PTZDefaultPositionSpeed="1" PTZDefaultFocusSpeed="0.5" PTZDefaultFocusEnabled="False" PTZDefaultTallyEnabled="False"
         PTZAlwaysShowThumbnail="False" VideoInterlaced="False" VideoShader_ColorCorrectionSourceEnabled="0" VideoShader_White="1" VideoShader_Black="0"
@@ -218,7 +236,7 @@ function getVideoXML(
         VideoShader_Deinterlace="False" VideoShader_Sharpen="False" VideoShader_ToleranceRed="0" VideoShader_ToleranceGreen="0" VideoShader_ToleranceBlue="0"
         VideoShader_ToleranceGreenGap="0" VideoShader_GreenFilter="False" VideoShader_GreenFilterTransparencyThreshold="1" VideoShader_LumaKeyThreshold="0"
         VideoShader_AntiAliasing="False" VideoShader_AntiAliasingFilter="0" VideoShader_ClippingX1="0" VideoShader_ClippingX2="1" VideoShader_ClippingY1="0"
-        VideoShader_ClippingY2="1" VideoShader_PremultipliedAlpha="False">${file.path}</Input>`;
+        VideoShader_ClippingY2="1" VideoShader_PremultipliedAlpha="False">${escapeXML(file.path)}</Input>`;
 }
 
 function getAudioXML(
@@ -226,11 +244,10 @@ function getAudioXML(
     layers: string[] = [],
     options: string[] = [],
 ) {
-    const layersText = layers.map((layer, i) => `Overlay${i}="${layer}"`).join('');
     const { volume, gain } = getAudioOptions(options);
     const loop = options.includes('loop') ? 'True' : 'False';
 
-    return `<Input Type="0" Position="0" RangeStart="0" RangeStop="0" State="1" OriginalTitle="${path.basename(file.path)}"
+    return `<Input Type="0" Position="0" RangeStart="0" RangeStop="0" State="1" OriginalTitle=""
         ShortcutMappings="" Key="${file.id}" Loop="${loop}" VolumeF="${volume}" Muted="True" BalanceF="0" AspectRatio="100"
         Category="0" MouseClickAction="0" GOClickAction="20" Collapsed="False" SoloPFLMode="False" MetersPF="False" Solo="False"
         BusMVolumeF="1" HeadphonesVolumeF="1" BusAVolumeF="1" BusBVolumeF="1" BusCVolumeF="1" BusDVolumeF="1" BusEVolumeF="1"
@@ -240,7 +257,7 @@ function getAudioXML(
         AudioEQGainDB0="0" AudioEQGainDB1="0" AudioEQGainDB2="0" AudioEQGainDB3="0" AudioEQGainDB4="0" AudioEQGainDB5="0"
         AudioEQGainDB6="0" AudioEQGainDB7="0" AudioEQGainDB8="0" AudioEQGainDB9="0" AudioAGCEnabled="False" AudioRackXML="&lt;plugins /&gt;"
         BusMaster="True" BusA="True" FrameDelay="0" TallyCOMPort="None" TallyNumber="0" AutoAudioMixing="True" AutoPause="False"
-        AutoRestart="False" AutoPlay="True" Mirror="False" SelectedIndex="0" Rate="1" FlattenLayers="False" ${layersText}
+        AutoRestart="False" AutoPlay="True" Mirror="False" SelectedIndex="0" Rate="1" FlattenLayers="False" ${getLayersText(layers)}
         XML="" ShaderSource="00000000-0000-0000-0000-000000000000" PTZProvider="" PTZConnection="" PTZAutoConnect="False"
         PTZDefaultPanTiltSpeed="0.5" PTZDefaultZoomSpeed="0.5" PTZDefaultPositionSpeed="1" PTZDefaultFocusSpeed="0.5"
         PTZDefaultFocusEnabled="False" PTZDefaultTallyEnabled="False" PTZAlwaysShowThumbnail="False" VideoInterlaced="False"
@@ -252,7 +269,7 @@ function getAudioXML(
         VideoShader_ToleranceGreen="0" VideoShader_ToleranceBlue="0" VideoShader_ToleranceGreenGap="0" VideoShader_GreenFilter="False"
         VideoShader_GreenFilterTransparencyThreshold="1" VideoShader_LumaKeyThreshold="0" VideoShader_AntiAliasing="False"
         VideoShader_AntiAliasingFilter="0" VideoShader_ClippingX1="0" VideoShader_ClippingX2="1" VideoShader_ClippingY1="0"
-        VideoShader_ClippingY2="1" VideoShader_PremultipliedAlpha="False">${file.path}</Input>`;
+        VideoShader_ClippingY2="1" VideoShader_PremultipliedAlpha="False">${escapeXML(file.path)}</Input>`;
 }
 
 function getPhotosXML(
@@ -260,16 +277,15 @@ function getPhotosXML(
     layers: string[] = [],
     options: string[] = [],
 ) {
-    const layersText = layers.map((layer, i) => `Overlay${i}="${layer}"`).join('');
     const slideshowTime = options.find((opt) => opt.endsWith('s')) ?? '10s';
     const time = parseInt(slideshowTime) ?? 10;
 
-    return `<Input Type="2" Position="0" RangeStart="0" RangeStop="0" State="1" OriginalTitle="${path.basename(file.path)}"
+    return `<Input Type="2" Position="0" RangeStart="0" RangeStop="0" State="1" OriginalTitle=""
         ShortcutMappings="" Key="${file.id}" Loop="True" VolumeF="1" Muted="True" BalanceF="0" AspectRatio="100" Category="0"
         MouseClickAction="0" GOClickAction="20" Collapsed="True" Solo="False" BusMVolumeF="1" HeadphonesVolumeF="1" BusAVolumeF="1"
         BusBVolumeF="1" BusCVolumeF="1" BusDVolumeF="1" BusEVolumeF="1" BusFVolumeF="1" BusGVolumeF="1" BusMaster="True" FrameDelay="0"
         TallyCOMPort="None" TallyNumber="0" AutoAudioMixing="True" AutoPause="True" AutoRestart="True" AutoPlay="False" Mirror="False"
-        SelectedIndex="1" Rate="1" FlattenLayers="False" ${layersText} XML="&lt;indexMappings /&gt;" ShaderSource="00000000-0000-0000-0000-000000000000"
+        SelectedIndex="1" Rate="1" FlattenLayers="False" ${getLayersText(layers)} XML="&lt;indexMappings /&gt;" ShaderSource="00000000-0000-0000-0000-000000000000"
         PTZProvider="" PTZConnection="" PTZAutoConnect="False" PTZDefaultPanTiltSpeed="0.5" PTZDefaultZoomSpeed="0.5" PTZDefaultPositionSpeed="1"
         PTZDefaultFocusSpeed="0.5" PTZDefaultFocusEnabled="False" PTZDefaultTallyEnabled="False" PTZAlwaysShowThumbnail="False"
         PictureTransition="${String(time)}" PictureDuration="500" PictureEffect="0" PictureBlackBorders="True" VideoShader_ColorCorrectionSourceEnabled="0"
@@ -280,19 +296,17 @@ function getPhotosXML(
         VideoShader_Sharpen="False" VideoShader_ToleranceRed="0" VideoShader_ToleranceGreen="0" VideoShader_ToleranceBlue="0"
         VideoShader_ToleranceGreenGap="0" VideoShader_GreenFilter="False" VideoShader_GreenFilterTransparencyThreshold="1"
         VideoShader_LumaKeyThreshold="0" VideoShader_AntiAliasing="False" VideoShader_AntiAliasingFilter="0" VideoShader_ClippingX1="0"
-        VideoShader_ClippingX2="1" VideoShader_ClippingY1="0" VideoShader_ClippingY2="1" VideoShader_PremultipliedAlpha="False">${file.path}</Input>`;
+        VideoShader_ClippingX2="1" VideoShader_ClippingY1="0" VideoShader_ClippingY2="1" VideoShader_PremultipliedAlpha="False">${escapeXML(file.path)}</Input>`;
 }
 
 function getVirtualInput(file: { id: string; newId: string }, layers: string[] = []) {
-    const layersText = layers.map((layer, i) => `Overlay${i}="${layer}"`).join('');
-
     return `<Input Type="22" Position="0" RangeStart="0" RangeStop="0" State="1" OriginalTitle="" 
         ShortcutMappings="" Key="${file.newId}" Loop="False" VolumeF="1" Muted="True" BalanceF="0" AspectRatio="100"
         Category="0" MouseClickAction="0" GOClickAction="20" Collapsed="False" Solo="False" BusMVolumeF="1"
         HeadphonesVolumeF="1" BusAVolumeF="1" BusBVolumeF="1" BusCVolumeF="1" BusDVolumeF="1" BusEVolumeF="1"
         BusFVolumeF="1" BusGVolumeF="1" BusMaster="True" FrameDelay="0"  TallyCOMPort="None" TallyNumber="0"
         AutoAudioMixing="True" AutoPause="True" AutoRestart="True" AutoPlay="True" Mirror="False" SelectedIndex="0"
-        Rate="1" FlattenLayers="False" ${layersText} XML="" ShaderSource="${file.id}"
+        Rate="1" FlattenLayers="False" ${getLayersText(layers)} XML="" ShaderSource="${file.id}"
         PTZProvider="" PTZConnection="" PTZAutoConnect="False" PTZDefaultPanTiltSpeed="0.5" PTZDefaultZoomSpeed="0.5"
         PTZDefaultPositionSpeed="1" PTZDefaultFocusSpeed="0.5" PTZDefaultFocusEnabled="False" PTZDefaultTallyEnabled="False"
         PTZAlwaysShowThumbnail="False" VirtualInputKey="${file.id}" UseSourceRenderEffects="True"
